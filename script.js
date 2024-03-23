7db3fcb027c9153d08fabe45ff7438cc375d7dcd
Jonh <jonh@example.com>
2024-03-23 13:37:22 +0200

    Start

---
+++ script.js
@@ -0,0 +1,9 @@
+//npm init // Команда, щоб додавати пакети різних розширень (СТАРТ)
+//npm init @eslint/config / / Додаємо розширення(наприклад ESlint)
+// Зробили валідацію коду для JS в налаштуваннях VSCode за допомогою ESlint
+
+const arr = [555];
+
+const aaa = 'fgdgf';
+
+const adssa = 'sdgdgdgfg';
